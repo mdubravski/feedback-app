d45331b9db1c6939f2d9355bded43764b8368a3e
mdubravski <mdubravski@gmail.com>
2022-01-26 21:05:21 -0500

    add state and useState hook example

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+
+// handling state
+function FeedbackItem() {
+  const [rating, setRating] = useState(7);
+  const [text, setText] = useState("This is an example of a feedback item");
+
+  return (
+    <div className="card">
+      <div className="num-display">{rating}</div>
+      <div className="text-display">{text}</div>
+    </div>
+  );
+}
+
+export default FeedbackItem;
